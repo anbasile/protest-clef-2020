ef19b1500a7d4e2219a1fedce3ec65a19c1074ab
abasile <me@angelobasile.it>
2020-11-04 21:55:07 +0100

    fixed name for inference

--- inference.py
+++ inference.py
@@ -23,12 +23,13 @@ class Inferencer:
             TODO
         """
 
-        self.model_type, self.pretrained_model, self.crf_decoding = model_dir.name.split('_')
-
+        self.model_type, self.pretrained_model, self.crf_decoding, self.encoding_mode = model_dir.name.split(
+            '_')
 
         self.input_file = input_file
 
-        self.output_file_name = input_file.with_suffix(f'.{self.model_type}-{self.pretrained_model}-{self.crf_decoding}')
+        self.output_file_name = input_file.with_suffix(
+            f'.{self.model_type}_{self.pretrained_model}_{self.crf_decoding}_{self.encoding_mode}')
 
         num_tags = 2 if self.model_type == 'classifier' else 19
 
@@ -54,10 +55,12 @@ class Inferencer:
                 17: 'O',
                 18: 'O'}
 
-        self.tokenizer = BertTokenizerFast.from_pretrained(self.pretrained_model)
+        self.tokenizer = BertTokenizerFast.from_pretrained(
+            self.pretrained_model)
 
         if self.model_type == 'tagger':
-            self.model = SequenceTagger(self.pretrained_model, num_tags, self.crf_decoding)
+            self.model = SequenceTagger(
+                self.pretrained_model, num_tags, self.crf_decoding)
         elif self.model_type == 'classifier':
             self.model = SequenceClassifier(self.pretrained_model, num_tags)
 
@@ -73,13 +76,15 @@ class Inferencer:
 
         df['ids'] = df.splits.apply(self.tokenizer.convert_tokens_to_ids)
 
-        df['sentence_id'] = df.token.str.contains('SAMPLE_START').astype(int).cumsum()-1
+        df['sentence_id'] = df.token.str.contains(
+            'SAMPLE_START').astype(int).cumsum()-1
 
         df = df[~df.token.isin(['SAMPLE_START', '[SEP]'])]
 
         sentence_grouped = df.groupby('sentence_id')
 
-        self.df = list(chain.from_iterable(np.array_split(g, math.ceil(g.ids.apply(len).sum()/510)) for _, g in sentence_grouped))
+        self.df = list(chain.from_iterable(np.array_split(g, math.ceil(
+            g.ids.apply(len).sum()/510)) for _, g in sentence_grouped))
 
         input_ids = [np.concatenate([
             np.array([101]),
@@ -87,9 +92,9 @@ class Inferencer:
             np.array([102])]) for chunk in self.df]
 
         encoded_data = tf.data.Dataset.from_tensor_slices({
-            'input_ids' : tf.ragged.constant(input_ids).to_tensor(0),
-            'attention_mask' : tf.ragged.constant([[1]*len(x) for x in input_ids]).to_tensor(0),
-            'token_type_ids' : tf.ragged.constant([[0]*len(x) for x in input_ids]).to_tensor(0),
+            'input_ids': tf.ragged.constant(input_ids).to_tensor(0),
+            'attention_mask': tf.ragged.constant([[1]*len(x) for x in input_ids]).to_tensor(0),
+            'token_type_ids': tf.ragged.constant([[0]*len(x) for x in input_ids]).to_tensor(0),
         })
 
         return encoded_data.batch(8)
@@ -105,19 +110,18 @@ class Inferencer:
             tmp = chunk.explode('ids')
             tmp['predictions'] = predictions[chunk_id][1:tmp.shape[0]+1]
             for n, g in tmp.groupby(tmp.index):
-                output_lines.append(f'{g.token.iloc[0]}\t{self.index2label[g.predictions.iloc[0]]}')
+                output_lines.append(
+                    f'{g.token.iloc[0]}\t{self.index2label[g.predictions.iloc[0]]}')
 
         with open(self.input_file, 'r') as f:
             for idx, line in enumerate(f):
                 if line.strip() in ['SAMPLE_START', '[SEP]']:
                     output_lines.insert(idx, f'{line.strip()}\tO')
                 elif line.strip() == '':
-                     output_lines.insert(idx, line.strip())
+                    output_lines.insert(idx, line.strip())
                 else:
                     pass
-                    
 
         with open(self.output_file_name, 'w') as f:
             for line in output_lines:
                 f.write(f'{line}\n')
-
