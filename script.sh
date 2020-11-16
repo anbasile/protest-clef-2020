6ae5cf0f8dd9e9945b05c797a9840e87c1f9bf4f
abasile <me@angelobasile.it>
2020-11-16 16:13:31 +0100

    fixed error in scrip file

--- script.sh
+++ script.sh
@@ -10,12 +10,12 @@ do
         else
             decoding_boolean="False"
         fi
-        protesta predict outputs/${model}_${decoding_boolean} /content/drive/My\ Drive/protesta-data/task3/test.tsv
-        protesta predict outputs/${model}_${decoding_boolean} /content/drive/My\ Drive/protesta-data/task3/china_test.data
-        mv /content/drive/My\ Drive/protesta-data/task3/test.tagger_${model}_${decoding_boolean} ./task3_test.predict
-        mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger_${model}_${decoding_boolean} ./china_test.predict
+        protesta predict outputs/${model}_${decoding_boolean}_${mode} /content/drive/My\ Drive/protesta-data/task3/test.tsv
+        protesta predict outputs/${model}_${decoding_boolean}_${mode} /content/drive/My\ Drive/protesta-data/task3/china_test.data
+        mv /content/drive/My\ Drive/protesta-data/task3/test.tagger_${model}_${decoding_boolean}_${mode} ./task3_test.predict
+        mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger_${model}_${decoding_boolean}_${mode} ./china_test.predict
         zip /content/drive/My\ Drive/protest-predictions-and-models/${model}_${decoding_boolean}.zip ./task3_test.predict ./china_test.predict
-        tar -cvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}.tar.gz outputs/tagger_${model}_${decoding_boolean}
+        tar -cvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}.tar.gz outputs/tagger_${model}_${decoding_boolean}_${mode}
         rm -rf outputs/ task3_test.predict ./china_test.predict
     done
     done
