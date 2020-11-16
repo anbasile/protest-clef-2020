c37d191c4b0e5303f6300523036b694e4bc8e136
abasile <me@angelobasile.it>
2020-11-16 16:06:40 +0100

    fixed error in scrip file

--- script.sh
+++ script.sh
@@ -12,9 +12,9 @@ do
         fi
         protesta predict outputs/${model}_${decoding_boolean} /content/drive/My\ Drive/protesta-data/task3/test.tsv
         protesta predict outputs/${model}_${decoding_boolean} /content/drive/My\ Drive/protesta-data/task3/china_test.data
-        mv /content/drive/My\ Drive/protesta-data/task3/test.tagger-${model}-${decoding_boolean} ./task3_test.predict
-        mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger-${model}-${decoding_boolean} ./china_test.predict
-        zip /content/drive/My\ Drive/protest-predictions-and-models/${model}-${decoding_boolean}.zip ./task3_test.predict ./china_test.predict
+        mv /content/drive/My\ Drive/protesta-data/task3/test.tagger_${model}_${decoding_boolean} ./task3_test.predict
+        mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger_${model}_${decoding_boolean} ./china_test.predict
+        zip /content/drive/My\ Drive/protest-predictions-and-models/${model}_${decoding_boolean}.zip ./task3_test.predict ./china_test.predict
         tar -cvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}.tar.gz outputs/tagger_${model}_${decoding_boolean}
         rm -rf outputs/ task3_test.predict ./china_test.predict
     done
