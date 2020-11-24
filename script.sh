62d190f6cb1bdc475b39523ac11e0c6fb9d4e6f0
Angelo <anbasile@users.noreply.github.com>
2020-11-24 21:14:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,7 +14,7 @@ do
         protesta predict outputs/${model}_${decoding_boolean}_${mode} /content/drive/My\ Drive/protesta-data/task3/china_test.data
         mv /content/drive/My\ Drive/protesta-data/task3/test.tagger_${model}_${decoding_boolean}_${mode} ./task3_test.predict
         mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger_${model}_${decoding_boolean}_${mode} ./china_test.predict
-        zip /content/drive/My\ Drive/protest-predictions-and-models/${model}_${decoding_boolean}.zip ./task3_test.predict ./china_test.predict
+        zip /content/drive/My\ Drive/protest-predictions-and-models/${model}_${decoding_boolean}_${mode}.zip ./task3_test.predict ./china_test.predict
         tar -cvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}.tar.gz outputs/tagger_${model}_${decoding_boolean}_${mode}
         rm -rf outputs/ task3_test.predict ./china_test.predict
     done
