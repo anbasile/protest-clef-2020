47691f34b7be6d7bd06374247bb5f0b64265a68a
abasile <me@angelobasile.it>
2020-12-28 15:33:00 +0100

    added script to run learning curve

---
+++ learning_curve.sh
@@ -0,0 +1,24 @@
+for model in bert-base-uncased protest-model
+do
+    for decoding in --crf-decoding
+    do 
+        for mode in document
+        do
+            for datasize in 0.1 0.3 0.5 0.7 0.9
+            do
+            protesta fit tagger $decoding $mode $model /content/drive/My\ Drive/protesta-data/task3/ $decoding $mode
+            if [ $decoding = "--crf-decoding" ]; then
+                decoding_boolean="True"
+            else
+                decoding_boolean="False"
+            fi
+            protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode} /content/drive/My\ Drive/protesta-data/task3/test.data
+            protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode} /content/drive/My\ Drive/protesta-data/task3/china_test.data
+            mv /content/drive/My\ Drive/protesta-data/task3/test.tagger_${model}_${decoding_boolean}_${mode}_${datasize} ./task3_test.predict
+            mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger_${model}_${decoding_boolean}_${mode}_${datasize} ./china_test.predict
+            zip /content/drive/My\ Drive/protest-predictions-and-models/${model}_${decoding_boolean}_${mode}_${datasize}.zip ./task3_test.predict ./china_test.predict
+            tar -cvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}_${datasize}.tar.gz outputs/tagger_${model}_${decoding_boolean}_${mode}_${datasize}
+            rm -rf outputs/ task3_test.predict ./china_test.predict
+    done
+    done
+done
