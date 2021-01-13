101173094a16d2f8fbad32e49d0b8b5adf2b4071
Angelo <anbasile@users.noreply.github.com>
2021-01-13 12:32:02 +0100

    Create get_predictions_from_dev.sh

---
+++ get_predictions_from_dev.sh
@@ -0,0 +1,21 @@
+for model in bert-base-uncased protest-model
+do
+    for decoding in --no-crf-decoding --crf-decoding
+    do 
+        for mode in sentence
+        do
+            mkdir outputs/
+            if [ $decoding = "--crf-decoding" ]; then
+                decoding_boolean="True"
+            else
+                decoding_boolean="False"
+            fi
+            cp /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}.tar.gz outputs/
+            tar xvfz outputs/tagger_${model}_${decoding_boolean}_${mode}.tar.gz -C outputs/
+            protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode}_${datasize} /content/drive/My\ Drive/protesta-data/task3/dev.data
+            mv /content/drive/My\ Drive/protesta-data/task3/dev.tagger_${model}_${decoding_boolean}_${mode} ./dev.predict
+            zip /content/drive/My\ Drive/protest-predictions-and-models/dev_${model}_${decoding_boolean}_${mode}.zip ./dev.predict
+            rm -rf outputs/
+    done
+    done
+done
