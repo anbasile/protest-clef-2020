9873a42685d12ef3908b897dbf85d1babd5da3a4
Angelo <anbasile@users.noreply.github.com>
2021-01-13 13:49:58 +0100

    Update get_predictions_from_dev.sh

--- get_predictions_from_dev.sh
+++ get_predictions_from_dev.sh
@@ -11,7 +11,8 @@ do
                 decoding_boolean="False"
             fi
             tar xvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}.tar.gz -C outputs/
-            protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode} /content/drive/My\ Drive/protesta-data/task3/dev.data
+            mv outputs/tagger_${model}_${decoding_boolean}_${mode} outputs/tagger_${model}_${decoding_boolean}_${mode}_1.0
+            protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode}_1.0 /content/drive/My\ Drive/protesta-data/task3/dev.data
             mv /content/drive/My\ Drive/protesta-data/task3/dev.tagger_${model}_${decoding_boolean}_${mode}_1.0 ./dev.predict
             zip /content/drive/My\ Drive/protest-predictions-and-models/dev_${model}_${decoding_boolean}_${mode}.zip ./dev.predict
             rm -rf outputs/
