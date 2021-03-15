8ce5969c1c8df09af99c00b73f3f658302ea1933
Angelo <anbasile@users.noreply.github.com>
2021-03-15 19:43:20 +0100

    Update get_predictions_from_dev.sh

--- get_predictions_from_dev.sh
+++ get_predictions_from_dev.sh
@@ -10,11 +10,11 @@ do
             else
                 decoding_boolean="False"
             fi
-            tar xvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}.tar.gz -C outputs/
+            tar xvf /content/drive/My\ Drive/protest-predictions-and-models_bert_retrain_epochs/tagger_${model}_${decoding_boolean}_${mode}.tar.gz -C outputs/
             mv outputs/outputs/tagger_${model}_${decoding_boolean}_${mode} outputs/tagger_${model}_${decoding_boolean}_${mode}_1.0
             protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode}_1.0 /content/drive/My\ Drive/protesta-data/task3/dev.data
             mv /content/drive/My\ Drive/protesta-data/task3/dev.tagger_${model}_${decoding_boolean}_${mode}_1.0 ./dev.predict
-            zip /content/drive/My\ Drive/protest-predictions-and-models/dev_${model}_${decoding_boolean}_${mode}.zip ./dev.predict
+            zip /content/drive/My\ Drive/protest-predictions-and-models_bert_retrain_epochs/dev_${model}_${decoding_boolean}_${mode}.zip ./dev.predict
             rm -rf outputs/
     done
     done
