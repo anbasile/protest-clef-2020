9b75e4f489a2d7e4cdd4712d50b25776c41ec7eb
Angelo <anbasile@users.noreply.github.com>
2021-03-19 11:17:02 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,8 +16,8 @@ do
             protesta predict outputs/tagger_${model}_${decoding_boolean}_${mode}_${datasize} /content/drive/My\ Drive/protesta-data/task3/china_test.data
             mv /content/drive/My\ Drive/protesta-data/task3/test.tagger_${model}_${decoding_boolean}_${mode}_${datasize} ./task3_test.predict
             mv /content/drive/My\ Drive/protesta-data/task3/china_test.tagger_${model}_${decoding_boolean}_${mode}_${datasize} ./china_test.predict
-            zip /content/drive/My\ Drive/protest-predictions-and-models/${model}_${decoding_boolean}_${mode}.zip ./task3_test.predict ./china_test.predict
-            tar -cvf /content/drive/My\ Drive/protest-predictions-and-models/tagger_${model}_${decoding_boolean}_${mode}_${datasize}.tar.gz outputs/tagger_${model}_${decoding_boolean}_${mode}_${datasize}
+            zip /content/drive/My\ Drive/protest-predictions-and-models_bert_retrain_epochs/${model}_${decoding_boolean}_${mode}.zip ./task3_test.predict ./china_test.predict
+            tar -cvf /content/drive/My\ Drive/protest-predictions-and-models_bert_retrain_epochs/tagger_${model}_${decoding_boolean}_${mode}_${datasize}.tar.gz outputs/tagger_${model}_${decoding_boolean}_${mode}_${datasize}
             rm -rf outputs/ task3_test.predict ./china_test.predict
     done
     done
